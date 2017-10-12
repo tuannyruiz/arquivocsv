3585960f10c957d4038fe29ce69ac2acd283de41
Tuanny Ruiz <tuannyruiz@gmail.com>
2017-10-11 21:27:07 -0300

    Código para criação de um cabeçalho no arquivo CSV

--- Program.cs
+++ Program.cs
@@ -18,9 +18,24 @@ namespace arquivocsv
             Console.WriteLine("Digite a sua idade: ");
             idade = Int16.Parse(Console.ReadLine());
             
-            // O 'true' serve para as novas informações serem adicionadas e não sobrescreverem o documento.
-            StreamWriter arquivo = new StreamWriter("dados.csv", true);
-            arquivo.WriteLine(nome + ";" + email + ";" + idade + ";" + DateTime.Now.ToShortDateString());
+            FileInfo fi = new FileInfo("dados_cabecalho.csv");
+            StreamWriter arquivo;
+
+            //Se o arquivo já existir, apenas adicione a nova linha no arquivo
+            if(fi.Exists)
+            {
+                // O 'true' serve para as novas informações serem adicionadas e não sobrescreverem o documento.
+                arquivo = new StreamWriter("dados_cabecalho.csv", true);
+                arquivo.WriteLine(nome + ";" + email + ";" + idade + ";" + DateTime.Now.ToShortDateString());
+            } 
+            //Se o arquivo não existe ainda, adicione um cabeçalho e então, adicione a linha com as informações
+            else 
+            {                
+                arquivo = new StreamWriter("dados_cabecalho.csv", true);
+                arquivo.WriteLine("Nome" + ";" + "E-mail" + ";" + "Idade" + ";" + "Data de Cadastro");
+                arquivo.WriteLine(nome + ";" + email + ";" + idade + ";" + DateTime.Now.ToShortDateString());
+            }
+
             arquivo.Close();
         }
     }
